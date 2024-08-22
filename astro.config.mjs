1bb246de99d8541a27ad377be71a71dfa452caf6
clpowe <clpowe@gmail.com>
2024-08-21 21:21:08 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig, passthroughImageService } from 'astro/config'
 import icon from 'astro-icon'
 
+import sitemap from '@astrojs/sitemap'
+
 import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
+	prefetch: {
+		prefetchAll: true
+	},
+	site: 'https://clpowe-astro-course.netlify.app/projects',
+	integrations: [icon(), sitemap()],
 	image: {
 		service: passthroughImageService()
 	},
